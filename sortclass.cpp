48c9b5c73a6d30bfd5b4e97a57e2fd8ba5f2e825
SaadBinAbiWaqas <P229322@pwr.nu.edu.pk>
2023-04-04 22:23:26 +0500

    sort update
    check it out

--- sortclass.cpp
+++ sortclass.cpp
@@ -1,69 +1,124 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
+#include <algorithm>
 #include <vector>
+#include<ctime>
+using namespace std;
 
-class Sort {
-    std::string name[3];
-    std::vector<std::string> dates;
-
+class task {
+    int id;
+    string title;
+    string description;
+ //   bool string s=comleted;
 public:
-    Sort() {
-        storeName();
-        storeDates();
-        sortName();
-        sortDates();
-        printNames();
-        printDates();
+    task(int id1 = 0, string s="", string d="") {
+        setid(id1);
+        setitle(s);
+        setdes(d);
     }
+    void setdes(string d)
+    {
+    description = d;
+}
+string getdes()
+{
+return description;
+}
+    void setitle(string s)
+    {
+    title=s;
+}
+string getitle()
+{
+return title;
+}
+    void setid(int id1) {
+        id = id1;
+    }
+    int getid() {
+        return id;
+    }
+    void print()
+    {
+    cout<<id<<endl;
+    cout<<title<<endl;
+    cout<<description<<endl;
+}
+};
 
-    void sortName() {
-        int Fletter;
-        int Sletter;
-        std::string temp;
-        for (int i = 0; i < 2; i++) {
-            if (name[i][0] > name[i + 1][0]) {
-                temp = name[i + 1];
-                name[i + 1] = name[i];
-                name[i] = temp;
+class Sortid {
+    task obj1[3];
+public:
+    Sortid() {
+    cout<<"enter ids with title"<<endl;
+    for(int i=0; i<3;i++)
+    {
+    int x;
+    cin>>x;
+    obj1[i].setid(x);
+    string s;
+    getline(cin>>ws,s);
+    obj1[i].setitle(s);
+    string d;
+    getline(cin>>ws,d);
+    obj1[i].setdes(d);
+}
+    }
+   void sortid() {
+    for (int i = 0; i < 2; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            if (obj1[i].getid() < obj1[j].getid()) {
+                task temp=obj1[i];
+                obj1[i]=obj1[j];
+                obj1[j]=temp;
             }
         }
     }
-
-    void printNames() {
-        for (int i = 0; i < 3; i++) {
-            std::cout << "Name of person: " << i + 1 << " " << name[i] << std::endl;
+}
+void sortdate() {
+    for (int i = 0; i < 2; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            time_t time = obj1[i].getDate();
+            time_t time1 = obj1[j].getDate();
+            double diff = difftime(time1, time);
+            if (diff < 0 ) {
+                task temp = obj1[i];
+                obj1[i] = obj1[j];
+                obj1[j] = temp;
+            }
         }
     }
-
-    void storeName() {
-        std::cout << "Enter names: " << std::endl;
-        for (int i = 0; i < 3; i++) {
-            std::cout << "enetr name of person " << i + 1 << std::endl;
-            std::getline(std::cin >> std::ws, name[i]);
+}
+void sortitle()
+{
+for (int i = 0; i < 2; i++) {
+        for (int j = i + 1; j < 3; j++) {
+        string var=obj1[i].getitle();
+        string var1=obj1[j].getitle();
+            if (var>var1) {
+                task temp=obj1[i];
+                obj1[i]=obj1[j];
+                obj1[j]=temp;
+            }
         }
     }
+}
 
-    void sortDates() {
-        sort(dates.begin(), dates.end(), std::greater<std::string>());
-    }
 
-    void printDates() {
-        for (int i = 0; i < dates.size(); i++) {
-            std::cout << dates[i] << std::endl;
-        }
-    }
+void print()
+{
+cout << "Sorted IDs (from greatest to smallest):" << endl;
+for(int i=0;i<3;i++)
+{
+obj1[i].print();
 
-    void storeDates() {
-        std::cout << "Enter dates (in any format):" << std::endl;
-        for (int i = 0; i < 3; i++) {
-            std::string date;
-            std::getline(std::cin >> std::ws, date);
-            dates.push_back(date);
-        }
-    }
+}
+}
+
 };
-
-int main() {
-    Sort object;
+int main()
+{
+Sortid obj;
+// obj.sortid();
+obj.sortitle();
+obj.print();
 }
